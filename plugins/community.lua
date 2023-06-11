1002adb0bacb898eefd5a7fd4b2e4547838dd822
HijiriKawai <hiji.k1124@gmail.com>
2023-06-12 02:51:00 +0900

    feat(plugins/community.lua): astrocommunityのプラグインを追加

---
+++ plugins/community.lua
@@ -0,0 +1,29 @@
+return {
+  "AstroNvim/astrocommunity",
+  {
+    import = "astrocommunity.colorscheme.catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+}
